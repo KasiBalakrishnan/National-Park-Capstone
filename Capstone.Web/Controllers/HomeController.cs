97b2b1fe17cf3962f06ccdb1e1e6b1119d56f4b1
Nathan Rathbun <naterathbun@gmail.com>
2017-03-08 11:46:20 -0500

    Added Main Index park list and basic CSS, hooks to go to detail view (TBI)

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Capstone.Web.DAL;
 
 namespace Capstone.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IParkDAL parkDAL;
+        
+        public HomeController(IParkDAL parkDAL)
+        {
+            this.parkDAL = parkDAL;
+        }
 
         // GET: Home
+        [HttpGet]
         public ActionResult Index()
         {
-            return View("Index");
+            return View("Index", parkDAL.GetParks());
         }
     }
 }
